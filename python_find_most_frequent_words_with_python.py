c63004fbd04cd0da1c5f44c3b723301a6cba5f7a
Tuvshuu <87944616+tuvvshe@users.noreply.github.com>
2023-08-01 23:35:49 +0200

    Add files via upload

---
+++ python_find_most_frequent_words_with_python.py
@@ -0,0 +1,10 @@
+
+words = []
+with open("sample3.txt", "r") as f:
+    for line in f:
+        words.extend(line.split())
+
+from collections import Counter
+counts = Counter(words)
+top5 = counts.most_common(5)
+print(top5)
